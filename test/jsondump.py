b0003f02fbcc4a118dad89dd0b81f1489d13d7ea
fengpinghu <fengping.hu@percolata.com>
2015-09-28 18:30:17 +0800

    modify files

--- test/jsondump.py
+++ test/jsondump.py
@@ -17,4 +17,3 @@ if __name__ == '__main__':
                                            title='My unit test',
                                            description='This demonstrates the report output by HTMLTestRunner.'
                                            )
-    
