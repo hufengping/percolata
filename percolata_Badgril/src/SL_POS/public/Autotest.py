e3c40faadb5c1b28fd0a8b1c51adf3f61ed7b26f
fengping <fengping.hu@percolata.com>
2015-10-14 23:17:21 +0800

    Percolata_badgril

--- percolata_Badgril/src/SL_POS/public/Autotest.py
+++ percolata_Badgril/src/SL_POS/public/Autotest.py
@@ -1,41 +1,30 @@
 #coding=utf-8
 '''
-Created on 2015年2月8日
-
 @author: fengping.hu
 '''
-import win32api, win32pdhutil, win32con,sys 
-import win32com.client
-from win32com.client import Dispatch
-import time,os,smtplib,string,datetime
+import time, os, smtplib, string, datetime
 from email.mime.text import MIMEText
 from email.header import Header
-import cx_Oracle
-#from public import db2file
-import db2file
 from stat import *
 from os import listdir
 from os.path import isdir
 
 
-#登录
-def login(self,username,password):
-    driver = self.driver
-    driver.find_element_by_id("j_username").clear()
-    driver.find_element_by_id("j_username").send_keys(username)
-    driver.find_element_by_id("j_password").clear()
-    driver.find_element_by_id("j_password").send_keys(password)
-    driver.find_element_by_xpath("/html/body/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[4]/input").click()
-#退出
-def logout(self):
-    driver = self.driver
-    driver.find_element_by_link_text(u"退出").click()
-    
-############################调用autoit3##########################
-try:
-    autoit = Dispatch("AutoItX3.Control")
-except:
-    print >> sys.stderr, u'AutoItX3 加载失败了！'
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import configparser
+
+
+# read credentials
+CREDENTIALS_FILE = '.credentials'
+cred = configparser.ConfigParser()
+cred.read(CREDENTIALS_FILE)
+
+GMAIL_USER = cred['GMAIL']['USER']
+GMAIL_PASSWD = cred['GMAIL']['PASSWD']
 
 #############################查找最新文件##########################    
 def newfile(result_dir):
@@ -54,59 +43,21 @@ def newfile(result_dir):
 def getTime(style = '%Y-%m-%d %H:%M:%S'):
     return time.strftime(style, time.localtime())
 
-def sendMail(mail_from, mail_to, mail_title, mail_content, mail_server=r'szhubap.sino-life.com', mail_port=25):
-    try:
-        handle = smtplib.SMTP(mail_server, mail_port)
-        #handle = smtplib.SMTP('mail.360.cn', 25)
-        handle.ehlo()  
-        handle.starttls()  
-        handle.ehlo()  
-        mail_data = getTime()
-        
-        #定义正文
-        f = open(mail_content, 'rb')
-        mail_body = f.read()
-        f.close()
-        
-        msg = "From: %s\r\nTo: %s\r\nData: %s\r\nContent-Type: text/html;charset=gb2312\r\nSubject: %s \r\n\r\n %s\r\n"  % (mail_from, str(mail_to).replace('[', '').replace(']', ''), mail_data, mail_title, mail_body)
-        #print msg
-        handle.sendmail('%s' % mail_from, mail_to, msg)
-        handle.close()
-    except Exception, e:
-        print('sendMail: '+str(e))
-        print '发信失败,程序退出...'
-        
-        
-###########################链接数据库############################   
-def conDatabase():
-    con = cx_Oracle.connect( "ovsee", "life12345","INT2")
-    cursor = con.cursor()
-    print cursor
-    cursor.close()
-    con.close() 
-###########################读写数据文件############################
-#写入数据文件
-def dataWrite(filepath,str):
-    
-    casefile = open(filepath,'w+')
-    casefile.write(str+'\n')
-    casefile.close()
+def mail(mail_to,subject, text):
+    """embed tracking code and mail to all emails"""
+    mailServer =smtplib.SMTP("smtp.gmail.com", 587)
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(GMAIL_USER, GMAIL_PASSWD)
 
-#读取数据文件首行    
-def dataRead(filepath1):
-    casefile1 = open(filepath1,'r')
-    return casefile1.readline()
-    casefile1.close()
+    msg = MIMEMultipart()
+    msg['From'] = GMAIL_USER
+    msg['Subject'] = subject
+    msg.attach(MIMEText(text, 'html'))
+    mailServer.sendmail(GMAIL_USER, mail_to, msg.as_string())
 
-#删除数据文件首行后重新写入
-def dataDelline(filepath2):
-    casefile2=open(filepath2) 
-    a=casefile2.readlines() 
-    fout=open(filepath2,'w') 
-    b=''.join(a[1:]) 
-    fout.write(b) 
-    casefile2.close() 
-    fout.close()  
+    mailServer.close()
+
 ###########################文件拷贝############################
 '''拷贝文件中修改日期为days天内的文件到指定目录'''
 def copynewfiles(source,target_dir,dayss=1):
@@ -129,21 +80,6 @@ def copynewfiles(source,target_dir,dayss=1):
         if days < string.atof(dayss) :
             os.system("copy %s %s" % (strr, trrr.strip()))
 
-    
-    
-    
-    
 if __name__ == '__main__':
-    autoit.MouseMove(100, 100)
-    #sendMail('badgirl@sino-life.com',['fengping.hu@sino-life.com'], '核心系统自动化运行结果', 'D:\\Workspaces\\python\\TestLogin126\\log\\2015-03-11 15_00_07result.html')
-    #conDatabase()
-    #print dataRead(db2file.case1datafile)
-    #dataDelline(db2file.case1datafile)
-    
-    inipath1 = os.path.split(os.path.realpath(__file__))[0] #获取当前路径
-    inipath2 = inipath1.split('src')[0]
-    print inipath2
-    source1 = inipath2+r"log" 
-    target_dir1 = inipath2+r"new"
-    
-    copynewfiles(source1,target_dir1,2)
+
+    mail("fengping.hu@percolata.com","test","test")
--- percolata_Badgril/src/SL_POS/run_05.bat
+++
@@ -1,3 +0,0 @@
-cd D:\shell\test\SL_POS\src\SL_POS
-D:
-python run_05_test.py >>D:\shell\test\SL_POS\log\log.txt 2>&1
